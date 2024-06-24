7781fa2277b1c26f4f7cc06b33094e098d66688c
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-06-24 15:30:18 +0100

    dashboard

--- app/components/Dashboard/Dashboard.tsx
+++ app/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,11 @@
 import { DashboardLayout } from "@/app/_layouts";
 import React from "react";
+import styles from "@/app/styles/Dashboard.module.scss";
 
 const Dashboard = () => {
   return (
     <DashboardLayout>
-      <section className="max-w-[1640px] flex flex-col gap-6 px-4 lg:px-8 py-8 md:h-screen overflow-y-scroll">
+      <section className={styles.container}>
         <h2>Dashboard</h2>
       </section>
     </DashboardLayout>
