f14a8fbc6a14d9c3572252616fdd6ab44b2e53ca
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-06-24 15:53:15 +0100

    page styles

--- app/components/Dashboard/pages/Users.tsx
+++ app/components/Dashboard/pages/Users.tsx
@@ -1,10 +1,11 @@
 import { DashboardLayout } from "@/app/_layouts";
 import React from "react";
+import styles from "@/app/styles/Users.module.scss";
 
 const Users = () => {
   return (
     <DashboardLayout>
-      <section className="max-w-[1640px] flex flex-col gap-6 px-4 lg:px-8 py-8 md:h-screen overflow-y-scroll">
+      <section className={styles.container}>
         <h2>Users</h2>
       </section>
     </DashboardLayout>
